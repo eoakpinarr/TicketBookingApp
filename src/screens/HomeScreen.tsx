44335bb17759b09ddfd2343b31ab6a9d1ddbc13a
eoakpinarr <eoakpinar@icloud.com>
2023-12-13 17:36:12 +0300

    CastCard, CategoryHeader, CustomIcon etc

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Dimensions,
+    TouchableOpacity,
+    ActivityIndicator,
+    ScrollView,
+    StatusBar,
+    FlatList,
+} from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
